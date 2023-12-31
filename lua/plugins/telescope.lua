b5cdf10d53192d52393b44cd3fa94f7b0253aa77
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2023-12-31 17:02:26 +0530

    Modularise vimrc

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,15 @@
+-- https://github.com/nvim-telescope/telescope.nvim#installation
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  -- or                          , branch = '0.1.x',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    -- https://github.com/nvim-telescope/telescope.nvim#usage
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+  end,
+}
+
---
+++ lua/vim/opt.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
