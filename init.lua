da306d0f7a1a52bb2fd7a6398ed8606e69f49575
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2023-12-31 15:42:14 +0530

    Add lazy plugin manager

--- init.lua
+++ init.lua
@@ -2,3 +2,22 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.tabstop = 2
+
+-- https://github.com/folke/lazy.nvim#-installation
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
