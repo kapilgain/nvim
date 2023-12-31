bf372e8dadc49de5c9741fc7b0fadc6d23c4d464
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2023-12-31 15:54:39 +0530

    Add telescope

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.g.mapleader = " "
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
@@ -19,7 +20,13 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
   -- https://github.com/catppuccin/nvim#configuration
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- https://github.com/nvim-telescope/telescope.nvim#installation
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 }
 local opts = {}
 
@@ -27,3 +34,10 @@ require("lazy").setup(plugins, opts)
 
 -- https://github.com/catppuccin/nvim#configuration
 vim.cmd.colorscheme "catppuccin"
+
+-- https://github.com/nvim-telescope/telescope.nvim#usage
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
