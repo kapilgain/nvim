93cef6ed28f803e68921300e1dd44b74334c42dd
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2023-12-31 17:02:44 +0530

    Add lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,7 @@
+-- https://github.com/nvim-lualine/lualine.nvim#lazynvim
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- https://github.com/nvim-lualine/lualine.nvim#starting-lualine
+  config = true,
+}
