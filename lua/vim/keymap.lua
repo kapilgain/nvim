713b28f5c66854d8f4fe6484a07c767ea508237e
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2024-01-01 21:18:45 +0530

    Lsp loves tabs. Guess we embrace it?

---
+++ lua/vim/keymap.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>x", "<cmd>bd<CR>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<leader>X", "<cmd>:silent !chmod +x %<CR>", { desc = "Make executable" })
--- lua/vim/opt.lua
+++ lua/vim/opt.lua
@@ -1,8 +1,17 @@
 vim.g.mapleader = " "
-vim.opt.clipboard:append("unnamedplus")
+
+-- Indentation
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", space = "·" }
+
+-- Clipboard
+vim.opt.clipboard:append({ "unnamedplus" })
+
+-- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
