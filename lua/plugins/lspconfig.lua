b55cff07687b5b03eed4c58425b8bbbfc918968d
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2024-01-02 19:39:48 +0530

    Update keymaps for telescope and lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -100,11 +100,9 @@ return {
           end, { buffer = ev.buf, desc = "List workspace folders" })
 
           vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "Type definition" })
-
+          vim.keymap.set("n", "<leader>gd", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Declaration" })
           vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
-
           vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf, desc = "Code action" })
-
           vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = ev.buf, desc = "References" })
           vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { buffer = ev.buf, desc = "Format" })
         end,
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,7 +10,8 @@ return {
       -- https://github.com/nvim-telescope/telescope.nvim#usage
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Git files" })
+      vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
       vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
       vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Old files" })
