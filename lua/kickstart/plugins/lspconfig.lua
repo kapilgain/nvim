ad70279d0b210e86a1c8edde3ce9c951d96917e0
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2024-10-16 16:24:19 +0530

    tsserver is deprecated; use ts_ls instead

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -225,9 +225,9 @@ return {
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- Typescript
-        tsserver = {},
+        ts_ls = {},
 
         -- Yaml
         yamlls = {},
