e8644ab1cbea2d5993a1287bfee1a2fa9cbd1f5b
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2025-10-04 18:41:58 +0530

    Disable snacks notifier and explorer; bring back neo-tree

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -6,13 +6,13 @@ return {
   opts = {
     bigfile = { enabled = true },
     -- dashboard = { enabled = true },
-    explorer = { enabled = true },
+    -- explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
-    notifier = {
-      enabled = true,
-      timeout = 3000,
-    },
+    -- notifier = {
+    --   enabled = true,
+    --   timeout = 3000,
+    -- },
     picker = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
