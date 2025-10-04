e2c5602c034d000ae6fe95885d6586d4a8131837
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2025-10-04 22:42:12 +0530

    Drop conflicting keymap

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -62,13 +62,13 @@ return {
       end,
       desc = "Notification History",
     },
-    {
-      "<leader>e",
-      function()
-        Snacks.explorer()
-      end,
-      desc = "File Explorer",
-    },
+    -- {
+    --   "<leader>e",
+    --   function()
+    --     Snacks.explorer()
+    --   end,
+    --   desc = "File Explorer",
+    -- },
     -- find
     {
       "<leader>fb",
