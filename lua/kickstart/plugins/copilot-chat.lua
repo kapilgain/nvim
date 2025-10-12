dc7dc632dc3d44203cb7f297e6c684102bdb5876
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2025-10-12 11:12:37 +0530

    Drop avante as it has issues with copilot; add copilot chat

---
+++ lua/kickstart/plugins/copilot-chat.lua
@@ -0,0 +1,19 @@
+-- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#lazynvim
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    cmd = "CopilotChat",
+    keys = {
+      { "<leader>at", "<cmd>CopilotChatToggle<cr>", desc = "Toggle Copilot Chat" },
+    },
+    config = function()
+      -- https://github.com/CopilotC-Nvim/CopilotChat.nvim?tab=readme-ov-file#chat-key-mappings
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set("i", "<S-Tab>", 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
+    end,
+  },
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -60,8 +60,13 @@ require("lazy").setup({
   -- require("kickstart.plugins.neorg"),
   require("kickstart.plugins.obsidian"),
   require("kickstart.plugins.trouble"),
+
   { "github/copilot.vim", cmd = { "Copilot" } },
-  require("kickstart.plugins.avante"),
+  -- Disable avante for now because of issues with copilot
+  -- https://github.com/yetone/avante.nvim/blob/v0.0.27/README.md?plain=1#L350-L353
+  -- https://github.com/yetone/avante.nvim/issues/1048
+  -- require("kickstart.plugins.avante"),
+  require("kickstart.plugins.copilot-chat"),
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
