efaf867d6c903f9ef63f797cac30802ff9bbe428
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2026-04-12 12:42:05 +0530

    Disable token counting in avante

--- lua/kickstart/plugins/avante.lua
+++ lua/kickstart/plugins/avante.lua
@@ -26,26 +26,29 @@ return {
           model = "gpt-4.1-2025-04-14",
           -- You can add other copilot-specific options here if needed
         },
+        --   claude = {
+        --     endpoint = "https://api.anthropic.com",
+        --     model = "claude-sonnet-4-20250514",
+        --     timeout = 30000, -- Timeout in milliseconds
+        --     extra_request_body = {
+        --       temperature = 0.75,
+        --       max_tokens = 20480,
+        --     },
+        --   },
+        --   moonshot = {
+        --     endpoint = "https://api.moonshot.ai/v1",
+        --     model = "kimi-k2-0711-preview",
+        --     timeout = 30000, -- Timeout in milliseconds
+        --     extra_request_body = {
+        --       temperature = 0.75,
+        --       max_tokens = 32768,
+        --     },
+        --   },
+        -- },
       },
-      --   claude = {
-      --     endpoint = "https://api.anthropic.com",
-      --     model = "claude-sonnet-4-20250514",
-      --     timeout = 30000, -- Timeout in milliseconds
-      --     extra_request_body = {
-      --       temperature = 0.75,
-      --       max_tokens = 20480,
-      --     },
-      --   },
-      --   moonshot = {
-      --     endpoint = "https://api.moonshot.ai/v1",
-      --     model = "kimi-k2-0711-preview",
-      --     timeout = 30000, -- Timeout in milliseconds
-      --     extra_request_body = {
-      --       temperature = 0.75,
-      --       max_tokens = 32768,
-      --     },
-      --   },
-      -- },
+      behaviour = {
+        enable_token_counting = false,
+      },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
