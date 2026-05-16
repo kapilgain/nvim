276efb99802a540e85b5f362414ac081c9e9f44f
Kapil Gain <69730236+kapilgain@users.noreply.github.com>
2026-05-16 15:59:50 +0530

    Add render-markdown

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -83,6 +83,7 @@ require("lazy").setup({
   require("kickstart.plugins.autopairs"),
   require("kickstart.plugins.neo-tree"),
   require("kickstart.plugins.flutter-tools"),
+  require("kickstart.plugins.render-markdown"),
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
